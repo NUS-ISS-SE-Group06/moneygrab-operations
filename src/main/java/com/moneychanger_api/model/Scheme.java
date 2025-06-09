e1a9e4c034248436042a17e49ff5aa12a615c857
Onggo Wahyudi <carnegzs@gmail.com>
2025-06-09 17:55:28 +0700

    fix: code scan -duplication

--- src/main/java/com/moneychanger_api/model/Scheme.java
+++ src/main/java/com/moneychanger_api/model/Scheme.java
@@ -22,18 +22,9 @@ public class Scheme {
     @Column(name = "is_default", nullable = false)
     private Boolean isDefault = false;
 
-    @Column(name = "created_at", nullable = false, updatable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
-    private Timestamp createdAt;
-
-    @Column(name = "updated_at", nullable = false, insertable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP")
-    private Timestamp updatedAt;
-
-    @Column(name = "created_by")
-    private Integer createdBy;
-
-    @Column(name = "updated_by")
     private Integer updatedBy;
 
-    @Column(name = "is_deleted", nullable = false, columnDefinition = "TINYINT(1) DEFAULT 0")
-    private Boolean isDeleted = false; 
+    @Column(name = "is_deleted", nullable = false, columnDefinition ="TINYINT(1) DEFAULT 0" )
+    private Boolean isDeleted=false;
+
 }
