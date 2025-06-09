53ae4ecada0eeee4913a48a692f6c6e8e1149dfc
Onggo Wahyudi <carnegzs@gmail.com>
2025-06-09 17:57:50 +0700

    fix code scan

--- src/main/java/com/moneychanger_api/model/Scheme.java
+++ src/main/java/com/moneychanger_api/model/Scheme.java
@@ -27,4 +27,16 @@ public class Scheme {
     @Column(name = "is_deleted", nullable = false, columnDefinition ="TINYINT(1) DEFAULT 0" )
     private Boolean isDeleted=false;
 
+    @Column(name = "created_at", nullable = false, updatable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    private Timestamp createdAt;
+
+    @Column(name = "updated_at", nullable = false, insertable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP")
+    private Timestamp updatedAt;
+
+    @Column(name = "created_by")
+    private Integer createdBy;
+
+    @Column(name = "updated_by")
+    private Integer updatedBy;
+
 }
