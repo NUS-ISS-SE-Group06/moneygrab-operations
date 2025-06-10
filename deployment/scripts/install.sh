4afe0414993281883faecddfab9a5467ac1d034f
sivabalaji1986 <42265473+sivabalaji1986@users.noreply.github.com>
2025-06-10 21:10:36 +0800

    Created Install.sh

---
+++ deployment/scripts/install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Install step (optional)"
+# Add any setup needed, such as clearing logs or moving files
+#!/bin/bash
+echo "Making scripts executable..."
+chmod +x /home/ec2-user/app/scripts/*.sh
