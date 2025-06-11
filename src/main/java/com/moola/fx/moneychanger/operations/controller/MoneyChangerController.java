ff47b134909e522a775f676056576b261719cecc
sivabalaji1986 <42265473+sivabalaji1986@users.noreply.github.com>
2025-06-11 13:13:13 +0800

    Updated money-changer api URL

--- src/main/java/com/moola/fx/moneychanger/operations/controller/MoneyChangerController.java
+++ src/main/java/com/moola/fx/moneychanger/operations/controller/MoneyChangerController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/moneychanger_api/moneychangers")
+@RequestMapping("/v1/money-changers")
 public class MoneyChangerController {
     private final MoneyChangerService service;
 
