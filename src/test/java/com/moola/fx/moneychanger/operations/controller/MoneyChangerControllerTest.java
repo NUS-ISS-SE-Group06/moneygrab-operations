538e851e750f323d21446359568c5849b0a49b52
salwhite <baiyfgeo2@yahoo.co.uk>
2025-06-14 17:30:33 +0800

    Refactor: fix SonarQube errors

--- src/test/java/com/moola/fx/moneychanger/operations/controller/MoneyChangerControllerTest.java
+++ src/test/java/com/moola/fx/moneychanger/operations/controller/MoneyChangerControllerTest.java
@@ -29,7 +29,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(MoneyChangerController.class)
-public class MoneyChangerControllerTest {
+class MoneyChangerControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
