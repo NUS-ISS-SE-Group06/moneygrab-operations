3f4974f40e3ce86894676c2a39bd5fbede475582
salwhite <baiyfgeo2@yahoo.co.uk>
2025-06-20 06:54:06 +0800

    Recified to github reviews

--- src/main/java/com/moola/fx/moneychanger/operations/controller/MoneyChangerController.java
+++ src/main/java/com/moola/fx/moneychanger/operations/controller/MoneyChangerController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("v1/money-changers")
+@RequestMapping("/v1/money-changers")
 public class MoneyChangerController {
 
     private final MoneyChangerService moneyChangerService;
