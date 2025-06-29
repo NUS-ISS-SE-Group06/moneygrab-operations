4abea63821bf99345acb78436ff4c70668576ec0
salwhite <baiyfgeo2@yahoo.co.uk>
2025-06-29 12:58:41 +0800

    fix sonar

--- src/main/java/com/moola/fx/moneychanger/operations/model/MoneyChangerPhoto.java
+++ src/main/java/com/moola/fx/moneychanger/operations/model/MoneyChangerPhoto.java
@@ -1,9 +1,11 @@
 package com.moola.fx.moneychanger.operations.model;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 @Entity
 @Table(name = "money_changer_photo")
+@Data
 public class MoneyChangerPhoto {
 
     @Id
@@ -25,54 +27,4 @@ public class MoneyChangerPhoto {
 
     @Column(name = "money_changer_id")
     private Long moneyChangerId;
-
-    // === Getters and Setters ===
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public byte[] getPhotoData() {
-        return photoData;
-    }
-
-    public void setPhotoData(byte[] photoData) {
-        this.photoData = photoData;
-    }
-
-    public String getPhotoFilename() {
-        return photoFilename;
-    }
-
-    public void setPhotoFilename(String photoFilename) {
-        this.photoFilename = photoFilename;
-    }
-
-    public String getPhotoMimetype() {
-        return photoMimetype;
-    }
-
-    public void setPhotoMimetype(String photoMimetype) {
-        this.photoMimetype = photoMimetype;
-    }
-
-    public Integer getIsDeleted() {
-        return isDeleted;
-    }
-
-    public void setIsDeleted(Integer isDeleted) {
-        this.isDeleted = isDeleted;
-    }
-
-    public Long getMoneyChangerId() {
-        return moneyChangerId;
-    }
-
-    public void setMoneyChangerId(Long moneyChangerId) {
-        this.moneyChangerId = moneyChangerId;
-    }
 }
