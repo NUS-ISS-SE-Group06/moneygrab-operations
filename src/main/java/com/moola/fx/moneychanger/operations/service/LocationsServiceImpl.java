b067f5067c9693d8fab9f61534b67d959b1c4a24
sivabalaji1986 <42265473+sivabalaji1986@users.noreply.github.com>
2025-07-12 13:25:03 +0800

    Implemented LocationsService methods in LocationsServiceImpl

---
+++ src/main/java/com/moola/fx/moneychanger/operations/service/LocationsServiceImpl.java
@@ -0,0 +1,43 @@
+package com.moola.fx.moneychanger.operations.service;
+
+import com.moola.fx.moneychanger.operations.dto.LocationDTO;
+import com.moola.fx.moneychanger.operations.model.Locations;
+import com.moola.fx.moneychanger.operations.repository.LocationRepository;
+
+import java.util.List;
+
+public class LocationsServiceImpl implements LocationsService{
+
+    private final LocationRepository locationRepository;
+
+    public LocationsServiceImpl(LocationRepository locationRepository) {
+        this.locationRepository = locationRepository;
+    }
+
+    @Override
+    public List<LocationDTO> listAllLocations() {
+        // Implementation to list all locations
+        return locationRepository.findAll().stream()
+                .filter(location -> Boolean.FALSE.equals(location.getIsDeleted()))
+                .map(this::toLocationDTO)
+                .toList();
+    }
+
+    @Override
+    public List<LocationDTO> findByCountryCode(String countryCode) {
+        // Implementation to find locations by country code
+        return locationRepository.findAll().stream()
+                .filter(location -> Boolean.FALSE.equals(location.getIsDeleted()))
+                .filter(location -> countryCode.equals(location.getCountryCode()))
+                .map(this::toLocationDTO)
+                .toList();
+    }
+
+    private LocationDTO toLocationDTO(Locations locationEntity) {
+        LocationDTO locationDTO = new LocationDTO();
+        locationDTO.setId(locationEntity.getId());
+        locationDTO.setLocationName(locationEntity.getLocationName());
+        locationDTO.setCountryCode(locationEntity.getCountryCode());
+        return locationDTO;
+    }
+}
